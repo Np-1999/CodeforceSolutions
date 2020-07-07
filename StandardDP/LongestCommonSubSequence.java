9e8c1be9dabd7d9787b2ad0592e9fa0443eeeb07
Np-1999 <nikhilpadole2014@gmail.com>
2020-07-07 20:12:53 +0530

    Description added for LongestCommonSubSequence

--- StandardDP/LongestCommonSubSequence.java
+++ StandardDP/LongestCommonSubSequence.java
@@ -1,3 +1,13 @@
+/*
+    Problem Statement: Given two sequences, find the length of longest subsequence present in both of them. 
+    A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. 
+    For example, "abc", "abg", "bdf", "aeg", "acefg", .. etc are subsequences of "abcdefg".
+    
+    Logic: If we found a match increment count and here we need not to set to count to 0 as we did
+    for substring as we are allowed to find more matching characters throughout the string.
+
+    And for dp if characters doesn't match we can copy max value from i-1 j or i j-1
+*/
 public class LongestCommonSubSequence {
     public static int lcs(String a, String b, int m, int n, int count) {
         if ( n==0 || m==0 ) {
@@ -6,7 +16,7 @@ public class LongestCommonSubSequence {
         else if( a.charAt(m-1)==b.charAt(n-1) ){
             return lcs(a, b, m-1, n-1, count+1);
         }
-        return Math.max(lcs(a, b, m-1, n, 0),lcs(a, b, m, n-1, count));
+        return Math.max(lcs(a, b, m-1, n, count),lcs(a, b, m, n-1, count));
     }
     public static int dp(String a, String b) {
         int m = a.length();
